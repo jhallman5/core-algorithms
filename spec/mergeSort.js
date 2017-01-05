f32fdad16a4f168842f5f0edcf0fa077b114c21f
John Hallman <jhallman5@Johns-MacBook-Pro.local>
2017-01-04 17:54:50 -0800

    mergeSort sorting method ALMOST works.

---
+++ spec/mergeSort.js
@@ -0,0 +1,16 @@
+import { expect } from 'chai'
+import mergeSort from '../src/mergeSort'
+
+describe('mergeSort', function(){
+  context('when given an array', function(){
+    it('should return a sorted array', function(){
+      expect(mergeSort([9,6,1,7])).to.be([1,6,7,9])
+      expect(mergeSort([9,-6,-1,7])).to.be([-6,-1,7,9])
+    })
+  })
+  context('when given a string', function(){
+    it('should return undefined', function(){
+      expect(mergeSort('string')).to.be(undefined)
+    })
+  })
+})
---
+++ src/mergeSort.js
@@ -0,0 +1,12 @@
+export default function mergeSort(array) {
+  // compare 0 with 1 then 2 with 3....
+//  compare the result of 0 & 1 with the result of 2 & 3 ...
+if (array[0] > array[1]
+}
+]
+[9, 6, 1, 7, 5, 4, 3, 2]
+[ [9], [6], [1], [7], [5], [4], [3], [2] ]
+
+[ [9 sort 6] , [1 sort 7] ,[ 5 sort 4], [3 sort 2] ]
+[ [6, 9 sort 1,7], [ 4, 5 sort 2, 3] ]
+[ 1, 6, 7, 9 sort 2, 3, 4, 5 ]
