b803bd14b4b62fb06144e2b12f50ecb3935cae79
John Hallman <jhallman5@Johns-MacBook-Pro.local>
2017-01-05 17:02:52 -0800

    mergesort function completed

--- src/mergeSort.js
+++ src/mergeSort.js
@@ -1,12 +1,54 @@
-export default function mergeSort(array) {
-  // compare 0 with 1 then 2 with 3....
-//  compare the result of 0 & 1 with the result of 2 & 3 ...
-if (array[0] > array[1]
-}
-]
-[9, 6, 1, 7, 5, 4, 3, 2]
-[ [9], [6], [1], [7], [5], [4], [3], [2] ]
+export {mergeSort};
 
-[ [9 sort 6] , [1 sort 7] ,[ 5 sort 4], [3 sort 2] ]
-[ [6, 9 sort 1,7], [ 4, 5 sort 2, 3] ]
-[ 1, 6, 7, 9 sort 2, 3, 4, 5 ]
+function makeArrayOfArrays(array) {
+  return array.map(function(member){
+    return [member]
+  })
+}
+
+function sortArrays(arrayA, arrayB) {
+  let results = [];
+//  console.log('SA: start', arrayA, arrayB)
+  while (Math.max(arrayA.length, arrayB.length) > 0){
+    // console.log('SA: arrayA', arrayA)
+    // console.log('SA: arrayB', arrayB)
+    let member
+    // if we have two members to compare
+    if (arrayA.length > 0 && arrayB.length > 0){
+      // take the smaller one
+      member = arrayA[0] < arrayB[0]
+        ? arrayA.shift()
+        : arrayB.shift()
+    }else{
+      // take whichever member exists
+      member = arrayA.length > 0
+        ? arrayA.shift()
+        : arrayB.shift()
+    }
+
+    // console.log('SA: member ->', member)
+    results.push(member)
+  }
+//  console.log('SA: results', results)
+  return results;
+}
+
+//actual merge sort
+function mergeSort(array) {
+  let arrayOfArrays = makeArrayOfArrays(array);
+  //console.log('MS: start', arrayOfArrays)
+  while(arrayOfArrays.length > 1){
+    for (var i = 0; i < arrayOfArrays.length; i++) {
+      // console.log('MS: start', {i})
+      // console.log('MS: before', arrayOfArrays)
+      const sets = arrayOfArrays.splice(i, 2)
+      // console.log('MS: sets', sets)
+      const sortedSets = sortArrays(sets[0], sets[1])
+      arrayOfArrays.splice(i, 0, sortedSets)
+      // console.log('MS: after', arrayOfArrays)
+    }
+  }
+  return arrayOfArrays[0]
+}
+
+//console.log('FINAL RESLT', mergeSort([10,30,40,3,2,4,6,7]))
