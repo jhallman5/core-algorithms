4ffac017ca6d534fca8e14adcf7308be1511622f
SAL <solomanasoloman@gmail.com>
2017-01-06 13:44:20 -0800

    Update fiobnacci.js

--- spec/fiobnacci.js
+++ spec/fiobnacci.js
@@ -1,13 +1,13 @@
 import { expect } from 'chai'
-import fiobnacci from '../src/fiobnacci'
+import fibonacci from '../src/fibonacci'
 
-describe('fiobnacci', function(){
+describe('fibonacci', function(){
   context('when given an number', function(){
-    it('should return a fiobnacci array', function(){
-      expect(fiobnacci(5)).to.be([0, 1, 1, 2, 3])
-      expect(fiobnacci(0)).to.be(undefined)
-      expect(fiobnacci(1)).to.be([0])
-      expect(fiobnacci(2)).to.be([0, 1])
+    it('should return a fibonacci array', function(){
+      expect(fibonacci(5)).to.be([0, 1, 1, 2, 3])
+      expect(fibonacci(0)).to.be(undefined)
+      expect(fibonacci(1)).to.be([0])
+      expect(fibonacci(2)).to.be([0, 1])
     })
   })
 })
